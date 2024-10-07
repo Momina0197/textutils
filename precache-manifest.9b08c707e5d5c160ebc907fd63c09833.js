f858490f3d82a4732e83d70969125d4dd1b46792
Momina0197 <Momina0197@users.noreply.github.com>
2024-10-07 17:43:29 +0500

    Updates

---
+++ precache-manifest.9b08c707e5d5c160ebc907fd63c09833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ce3831c81f6e9043c128f5313478d6",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "28b2bf10f4736e4d0ffe",
+    "url": "/textutils/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "c4629e1002fe060eb3c3",
+    "url": "/textutils/static/js/2.c76f9fbf.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/textutils/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "28b2bf10f4736e4d0ffe",
+    "url": "/textutils/static/js/main.7f437ede.chunk.js"
+  },
+  {
+    "revision": "9da5d41238847da73de8",
+    "url": "/textutils/static/js/runtime~main.1db17817.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/textutils/static/media/logo.06e73328.svg"
+  }
+]);
